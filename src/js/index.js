9b92aa2a5986acec93755e3882af63de24a8c09d
web-dev <nathankim137311@gmail.com>
2021-11-13 22:24:04 -0800

    added functions in task.js, and added functionality where if user clicks on add task button, task elements are displayed on screen

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,12 @@
-const addTaskBtn = document.getElementById('add-task');
+import showForm from "./animation.js";
+import { createTask, createTaskDom } from "./task.js";
+const addTaskLink = document.getElementById('add-task-link');
+const addTaskBtn = document.getElementById('add-task-btn')
 
-addTaskBtn.addEventListener('click', (e) => {
+addTaskLink.addEventListener('click', (e) => {
     showForm(e);
 });
 
-function showForm(e) {
-    const string = e.target.textContent;
-    if(string === 'Add Task') {
-        e.target.style.backgroundColor = 'red';
-        e.target.textContent = 'Close';
-        document.getElementById('form-container').classList.add('active');
-    } else if(string === 'Close') {
-        e.target.style.backgroundColor = '#5865F2'; // blurple
-        e.target.textContent = 'Add Task'
-        document.getElementById('form-container').classList.remove('active');
-    }
-}
-
-console.log('working')
+addTaskBtn.addEventListener('click', () => {
+    createTask();
+});
