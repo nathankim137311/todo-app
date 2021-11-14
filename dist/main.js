ac8c55736f79d543ff3a74bbe28a59c75200317c
web-dev <nathankim137311@gmail.com>
2021-11-13 18:54:17 -0800

    added form file and added feature that toggles form close and form open

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-console.log("hey what is going bro?");
+document.getElementById("add-task").addEventListener("click",(t=>{!function(t){const e=t.target.textContent;"Add Task"===e?(t.target.style.backgroundColor="red",t.target.textContent="Close",document.getElementById("form-container").classList.add("active")):"Close"===e&&(t.target.style.backgroundColor="#5865F2",t.target.textContent="Add Task",document.getElementById("form-container").classList.remove("active"))}(t)})),console.log("working");
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
-console.log('hey what is going bro?');
+const addTaskBtn = document.getElementById('add-task');
+
+addTaskBtn.addEventListener('click', (e) => {
+    showForm(e);
+});
+
+function showForm(e) {
+    const string = e.target.textContent;
+    if(string === 'Add Task') {
+        e.target.style.backgroundColor = 'red';
+        e.target.textContent = 'Close';
+        document.getElementById('form-container').classList.add('active');
+    } else if(string === 'Close') {
+        e.target.style.backgroundColor = '#5865F2'; // blurple
+        e.target.textContent = 'Add Task'
+        document.getElementById('form-container').classList.remove('active');
+    }
+}
+
+console.log('working')
